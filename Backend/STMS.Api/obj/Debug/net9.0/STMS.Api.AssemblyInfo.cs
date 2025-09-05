9561f537329843ef7f361b0edb070822218da07c
Nadil-Dulran <it23161788@my.sliit.lk>
2025-09-05 15:07:26 +0530

    Structure update

--- Backend/STMS.Api/obj/Debug/net9.0/STMS.Api.AssemblyInfo.cs
+++ Backend/STMS.Api/obj/Debug/net9.0/STMS.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df013b1d0a7a10cd8ecd26224f9831f8a689a6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3fda4e7545ea709a77c4eec9300c3b863ea23b")]
 [assembly: System.Reflection.AssemblyProductAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
