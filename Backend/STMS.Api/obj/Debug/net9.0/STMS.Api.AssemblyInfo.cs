5530955d881e441c6be126eb32a02da68d091d0d
Nadil-Dulran <it23161788@my.sliit.lk>
2025-09-11 17:45:04 +0530

    Added UniversityModel Tests

--- Backend/STMS.Api/obj/Debug/net9.0/STMS.Api.AssemblyInfo.cs
+++ Backend/STMS.Api/obj/Debug/net9.0/STMS.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb89016521003549597b52199733f0315c59419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e323f030b6e5d766168f897e094a6a2d1f1401e")]
 [assembly: System.Reflection.AssemblyProductAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
