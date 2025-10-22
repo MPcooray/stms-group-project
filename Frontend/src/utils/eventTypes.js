ca837250e91ccbbbcd330e9e4f6ce06a0460e43a
Nadil-Dulran <it23161788@my.sliit.lk>
2025-10-22 21:41:23 +0530

    Update event types used by admin

---
+++ Frontend/src/utils/eventTypes.js
@@ -0,0 +1,19 @@
+// Common event types used by admin when adding events to a tournament
+const eventTypes = [
+  "100m Freestyle Men",
+  "100m Freestyle Women",
+  "200m Freestyle Men",
+  "200m Freestyle Women",
+  "400m Freestyle Men",
+  "400m Freestyle Women",
+  "100m Backstroke Men",
+  "100m Backstroke Women",
+  "200m Butterfly Men",
+  "200m Butterfly Women",
+  "4x100m Freestyle Relay Men",
+  "4x100m Freestyle Relay Women",
+  "200m IM Men",
+  "200m IM Women",
+]
+
+export default eventTypes
